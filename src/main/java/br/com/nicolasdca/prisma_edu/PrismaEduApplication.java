4655283392c68ba4c9218dacdef687b316d030dd
nicolasdca <nicolasdavid193@gmail.com>
2025-06-18 14:52:49 -0300

    Spring Initial Setup

---
+++ src/main/java/br/com/nicolasdca/prisma_edu/PrismaEduApplication.java
@@ -0,0 +1,13 @@
+package br.com.nicolasdca.prisma_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrismaEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nicolasdca/prisma_edu/PrismaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolasdca.prisma_edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
